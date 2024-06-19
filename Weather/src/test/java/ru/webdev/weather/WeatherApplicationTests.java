28bd17fc77e2798ccb1f10dcc95a3dc7f3cb229f
Evgeny Bayandin <5087081@gmail.com>
2024-06-19 12:40:17 +0300

    Add module Weather

---
+++ Weather/src/test/java/ru/webdev/weather/WeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.webdev.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
