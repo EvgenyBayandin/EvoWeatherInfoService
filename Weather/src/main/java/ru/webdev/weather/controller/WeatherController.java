b5fe2be3e77324486da7c833f7a4f9c43f252c2a
Evgeny Bayandin <5087081@gmail.com>
2024-06-19 13:30:22 +0300

    Update WeatherController, modified @GetMapping("/weather"), for String request and return restTemplate.getForObject(request, Root.class).getMain(); Set custom server-port.

--- Weather/src/main/java/ru/webdev/weather/controller/WeatherController.java
+++ Weather/src/main/java/ru/webdev/weather/controller/WeatherController.java
@@ -3,8 +3,10 @@ package ru.webdev.weather.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import ru.webdev.weather.model.Main;
 import ru.webdev.weather.model.Root;
 
 @RestController
@@ -14,11 +16,17 @@ public class WeatherController {
     private RestTemplate restTemplate;
 
     @Value("${appid}")
-    String appid;
+    String appId;
+
+    @Value("${url.weather}")
+    String urlWeather;
 
     @GetMapping("/weather")
-    public Root getWeather() {
-        return restTemplate.getForObject("https://api.openweathermap.org/data/2.5/weather?lat=54.1838&lon=45.1749&units=metric&appid=" + appid, Root.class);
+    public Main getWeather(@RequestParam String lat, @RequestParam String lon) {
+
+        String request = String.format("%s?lat=%s&lon=%s&units=metric&appid=%s", urlWeather, lat, lon, appId);
+
+        return restTemplate.getForObject(request, Root.class).getMain();
     }
 
 }
