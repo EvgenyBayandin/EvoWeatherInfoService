18786e1516f848b6c9d6e1c159a239399489edcb
Evgeny Bayandin <5087081@gmail.com>
2024-06-19 13:08:59 +0300

    Add WeatherController.java

---
+++ Weather/src/main/java/ru/webdev/weather/controller/WeatherController.java
@@ -0,0 +1,22 @@
+package ru.webdev.weather.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import ru.webdev.weather.model.Root;
+
+@RestController
+public class WeatherController {
+
+    RestTemplate restTemplate = new RestTemplate();
+
+    @Value("${appid}")
+    String appid;
+
+    @GetMapping("/weather")
+    public Root getWeather(){
+        return restTemplate.getForObject("https://api.openweathermap.org/data/2.5/weather?lat=54.1838&lon=45.1749&units=metric&appid=" + appid, Root.class);
+    }
+
+}
