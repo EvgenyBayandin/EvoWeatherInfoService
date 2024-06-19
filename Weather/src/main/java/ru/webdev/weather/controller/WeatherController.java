b898c899c9d7a8287583926f6fc9e09cd0945525
Evgeny Bayandin <5087081@gmail.com>
2024-06-19 13:15:07 +0300

    Update WeatherController, add @Autowired RestTemplate. Set default server-port.

--- Weather/src/main/java/ru/webdev/weather/controller/WeatherController.java
+++ Weather/src/main/java/ru/webdev/weather/controller/WeatherController.java
@@ -1,5 +1,6 @@
 package ru.webdev.weather.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,13 +10,14 @@ import ru.webdev.weather.model.Root;
 @RestController
 public class WeatherController {
 
-    RestTemplate restTemplate = new RestTemplate();
+    @Autowired
+    private RestTemplate restTemplate;
 
     @Value("${appid}")
     String appid;
 
     @GetMapping("/weather")
-    public Root getWeather(){
+    public Root getWeather() {
         return restTemplate.getForObject("https://api.openweathermap.org/data/2.5/weather?lat=54.1838&lon=45.1749&units=metric&appid=" + appid, Root.class);
     }
 
