f8e22d3a4e64d3cdcd81f079dd0fe860b1eb38b2
Evgeny Bayandin <5087081@gmail.com>
2024-06-19 12:49:07 +0300

    Add class Clouds with Lombok annotations

---
+++ Weather/src/main/java/ru/webdev/weather/model/Clouds.java
@@ -0,0 +1,14 @@
+package ru.webdev.weather.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Clouds {
+
+    private int all;
+
+}
