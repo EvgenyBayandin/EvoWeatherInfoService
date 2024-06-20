2d8a4027013d9271ab8fa5ae4709fce24e517765
Evgeny Bayandin <5087081@gmail.com>
2024-06-20 10:57:44 +0300

    Modified LocationController, add @GetMapping("/weather") redirectRequestWeather(@RequestParam String name)

--- Location/src/main/java/ru/webdev/location/controller/LocationController.java
+++ Location/src/main/java/ru/webdev/location/controller/LocationController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import ru.webdev.location.model.Geodata;
+import ru.webdev.location.model.Weather;
 import ru.webdev.location.repository.GeodataRepository;
 
 @RestController("/location")
@@ -15,6 +17,15 @@ public class LocationController {
 
     @Autowired
     private GeodataRepository repository;
+
+    private RestTemplate restTemplate = new RestTemplate();
+
+    @GetMapping("/weather")
+    public Weather redirectRequestWeather(@RequestParam String name){
+        Geodata geodata = repository.findByName(name).get();
+        String url = String.format("http://localhost:8082/weather?lat=%s&lon=%s", geodata.getLat(), geodata.getLon());
+        return restTemplate.getForObject(url, Weather.class);
+    }
 
     @GetMapping
     public Optional<Geodata> getLocation(@RequestParam String name) {
