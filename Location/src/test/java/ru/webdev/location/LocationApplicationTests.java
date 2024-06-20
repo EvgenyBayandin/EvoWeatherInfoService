018f33de8be32cc138dfa40f34962b091cb0a546
Evgeny Bayandin <5087081@gmail.com>
2024-06-20 09:00:41 +0300

    Update GeodataRepository.

---
+++ Location/src/test/java/ru/webdev/location/LocationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.webdev.location;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
