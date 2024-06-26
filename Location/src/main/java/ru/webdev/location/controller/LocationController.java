5623f63687c15875f81de83c563bc7e4ef53fcb1
Evgeny Bayandin <5087081@gmail.com>
2024-06-26 22:57:43 +0300

    Update LocationController.java

--- Location/src/main/java/ru/webdev/location/controller/LocationController.java
+++ Location/src/main/java/ru/webdev/location/controller/LocationController.java
@@ -1,10 +1,10 @@
 package ru.webdev.location.controller;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import ru.webdev.location.model.Location;
@@ -21,41 +21,62 @@ public class LocationController {
     @Autowired
     private RestTemplate restTemplate;
 
+
     @GetMapping("/weather")
-    public Weather redirectRequestWeather(@RequestParam String name){
-        Location geodata = repository.findByName(name).get();
-        String url = String.format("http://localhost:8082/weather?lat=%s&lon=%s", geodata.getLatitude(), geodata.getLongitude());
-        return restTemplate.getForObject(url, Weather.class);
+    public ResponseEntity<Weather> redirectRequestWeather(@RequestParam String name) {
+        if (repository.findByName(name).isPresent()) {
+            Location geodata = repository.findByName(name).get();
+            String url = String.format("http://localhost:8082/weather?lat=%s&lon=%s", geodata.getLatitude(), geodata.getLongitude());
+            Weather weather = restTemplate.getForObject(url, Weather.class);
+            return new ResponseEntity(weather, HttpStatus.OK);
+        }
+        return new ResponseEntity("Location by name: " + name + " not found.", HttpStatus.NOT_FOUND);
     }
 
+
     @GetMapping
-    public List<Location> findAll() {
-        return new ArrayList<>((Collection) repository.findAll());
+    public ResponseEntity<List<Location>> findAll() {
+        return repository.findAll().iterator().hasNext()
+                ? new ResponseEntity(repository.findAll(), HttpStatus.OK)
+                : new ResponseEntity("No locations found.", HttpStatus.NOT_FOUND);
     }
 
     @GetMapping(params = "name")
-    public Optional<Location> getLocation(@RequestParam String name) {
-        return repository.findByName(name);
+    public ResponseEntity<Optional<Location>> getLocation(@RequestParam String name) {
+        return repository.findByName(name).isPresent()
+                ? new ResponseEntity(repository.findByName(name), HttpStatus.OK)
+                : new ResponseEntity("Location by name: " + name + " not found.", HttpStatus.NOT_FOUND);
     }
 
     @PostMapping
-    public Location save(@RequestBody Location location) {
-        return repository.save(location);
+    public ResponseEntity<Location> save(@RequestBody Location location) {
+        return repository.findByName(location.getName()).isPresent()
+                ? new ResponseEntity(repository.findByName(location.getName()), HttpStatus.BAD_REQUEST)
+                : new ResponseEntity(repository.save(location), HttpStatus.CREATED);
+
     }
 
     @PutMapping
-    public Location putLocation(@RequestBody Location location, @RequestParam String name)  {
-        Location existing = repository.findByName(name).get();
-        existing.setName(location.getName());
-        existing.setLatitude(location.getLatitude());
-        existing.setLongitude(location.getLongitude());
-        return repository.save(existing);
+    public ResponseEntity<Location> putLocation(@RequestBody Location location, @RequestParam String name) {
+        if (repository.findByName(name).isPresent()) {
+            Location existing = repository.findByName(name).get();
+            existing.setName(location.getName());
+            existing.setLatitude(location.getLatitude());
+            existing.setLongitude(location.getLongitude());
+            repository.save(existing);
+            return new ResponseEntity(existing, HttpStatus.OK);
+        }
+        return new ResponseEntity("Location by name: " + name + " not found.", HttpStatus.NOT_FOUND);
     }
 
     @DeleteMapping
-    public void deleteLocation(@RequestParam String name)   {
-        Location existing = repository.findByName(name).get();
-        repository.delete(existing);
+    public ResponseEntity<Location> deleteLocation(@RequestParam String name) {
+        if (repository.findByName(name).isPresent()) {
+            Location existing = repository.findByName(name).get();
+            repository.delete(existing);
+            return new ResponseEntity(existing, HttpStatus.OK);
+        }
+        return new ResponseEntity("Location by name: " + name + " not found", HttpStatus.NOT_FOUND);
     }
 
 }
