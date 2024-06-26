4565f02789b15224cb9bbcf18b59def9b7f549e3
Evgeny Bayandin <5087081@gmail.com>
2024-06-26 23:40:55 +0300

    5.8. Task 22, subtasks 1,2,3 done. Add Eureka-server-client settings in to Person-service, Location-service. Add @Bean RestTemplate in LocationConfig, PersonConfig. Refactoring Person.java Add @PutMapping, @DeleteMapping in PersonController.java

--- Person/src/main/java/ru/webdev/person/controller/PersonController.java
+++ Person/src/main/java/ru/webdev/person/controller/PersonController.java
@@ -4,12 +4,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import ru.webdev.person.model.Person;
 import ru.webdev.person.model.Weather;
@@ -26,31 +21,61 @@ public class PersonController {
     private RestTemplate restTemplate;
 
     @GetMapping
-    public Iterable<Person> findAll(){
-        return repository.findAll();
+    public ResponseEntity<Iterable<Person>> findAll() {
+        if (repository.findAll().iterator().hasNext()) {
+            return new ResponseEntity(repository.findAll(), HttpStatus.OK);
+        }
+        return new ResponseEntity("No persons found", HttpStatus.NOT_FOUND);
     }
 
     @GetMapping("/{id}")
-    public Optional<Person> findById(@PathVariable int id){
-        return repository.findById(id);
+    public ResponseEntity<Optional<Person>> findById(@PathVariable int id) {
+        if (repository.existsById(id)) {
+            return new ResponseEntity(repository.findById(id), HttpStatus.OK);
+        }
+        return new ResponseEntity("Person by ID: " + id + " not found.", HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping
+    public ResponseEntity<Person> save(@RequestBody Person person) {
+        return repository.findById(person.getId()).isPresent()
+                ? new ResponseEntity(repository.findById(person.getId()), HttpStatus.BAD_REQUEST)
+                : new ResponseEntity(repository.save(person), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Person> update(@PathVariable int id, @RequestBody Person person) {
+        if (repository.existsById(id)) {
+            Person p = repository.findById(id).get();
+            p.setFirstname(person.getFirstname());
+            p.setLastname(person.getLastname());
+            p.setSurname(person.getSurname());
+            p.setBirthdate(person.getBirthdate());
+            p.setLocation(person.getLocation());
+            repository.save(p);
+            return new ResponseEntity(p, HttpStatus.OK);
+        }
+        return new ResponseEntity("Person by ID: " + id + " not found.", HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Person> delete(@PathVariable int id) {
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+            return new ResponseEntity(HttpStatus.OK);
+        }
+        return new ResponseEntity("Person by ID: " + id + " not found.", HttpStatus.NOT_FOUND);
     }
 
     @GetMapping("/{id}/weather")
-    public ResponseEntity<Weather> getWeather(@PathVariable int id){
-        if(repository.existsById(id)){
+    public ResponseEntity<Weather> getWeather(@PathVariable int id) {
+        if (repository.existsById(id)) {
             String location = repository.findById(id).get().getLocation();
             Weather weather = restTemplate.getForObject("http://localhost:8083/location/weather?name=" + location, Weather.class);
             return new ResponseEntity(weather, HttpStatus.OK);
         }
-        return new ResponseEntity(null, HttpStatus.NOT_FOUND);
+        return new ResponseEntity("Person by ID: " + id + " not found.", HttpStatus.NOT_FOUND);
     }
 
-
-    @PostMapping
-    public ResponseEntity<Person> save(@RequestBody Person person){
-        return repository.findById(person.getId()).isPresent()
-                ? new ResponseEntity(repository.findById(person.getId()),HttpStatus.BAD_REQUEST)
-                : new ResponseEntity(repository.save(person), HttpStatus.CREATED);
-    }
 
 }
